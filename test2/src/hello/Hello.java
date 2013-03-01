f49d5b0ebdb335634d0ee1944fb5d185e9a6944b
subarusti <caaron@pocza.onet.pl>
2013-03-02 00:58:19 +0100

    kilka prostych prog

--- test2/src/hello/Hello.java
+++ test2/src/hello/Hello.java
@@ -1,15 +1,17 @@
 package hello;
+
 import java.awt.*;
 import java.applet.*;
-public class Hello extends Applet
-{
-	public void paint (Graphics g)
-	{
-		g.drawString("hi Ian T. zmiana", 20, 20 );	
+
+public class Hello extends Applet {
+	public void paint(Graphics g) {
+		String zmienna = "kartka";
+		// g.drawString("hi Ian T. zmiana", 20, 20 );
 		// komentarze
-		/*mozna zastosowac
-		  dluzsze komenty
+		/*
+		 * mozna zastosowac dluzsze komenty
 		 */
-		System.out.println("inny sposob wyswietlania");
+		System.out.println(zmienna + "\nzeszyt" );
+		// System.out.println();
 	}
 }
