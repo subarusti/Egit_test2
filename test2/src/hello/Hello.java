9e8276550c77d8256a58abbe3071046406008785
subarusti <caaron@pocza.onet.pl>
2013-03-01 23:19:26 +0100

    zmiana

--- test2/src/hello/Hello.java
+++ test2/src/hello/Hello.java
@@ -1,9 +1,10 @@
+package hello;
 import java.awt.*;
 import java.applet.*;
 public class Hello extends Applet
 {
 	public void paint (Graphics g)
 	{
-		g.drawString("hi Ian T.", 20, 20 );		
+		g.drawString("hi Ian T. zmiana", 20, 20 );		
 	}
 }
