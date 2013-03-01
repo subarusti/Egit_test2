8e9fcadf24bb8e6f23910c62d2c03b248c05159f
subarusti <caaron@pocza.onet.pl>
2013-03-01 23:30:15 +0100

    komenty

--- test2/src/hello/Hello.java
+++ test2/src/hello/Hello.java
@@ -5,6 +5,11 @@ public class Hello extends Applet
 {
 	public void paint (Graphics g)
 	{
-		g.drawString("hi Ian T. zmiana", 20, 20 );		
+		g.drawString("hi Ian T. zmiana", 20, 20 );	
+		// komentarze
+		/*mozna zastosowac
+		  dluzsze komenty
+		 */
+		System.out.println("inny sposob wyswietlania");
 	}
 }
