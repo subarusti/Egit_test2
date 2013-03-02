06d604d0a32b6cbe41220a9970863f4062fd94cb
subarusti <caaron@pocza.onet.pl>
2013-03-02 23:37:23 +0100

    rzutowanie

---
+++ test2/src/hello/Rzutowanie.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Rzutowanie {
+	public static void main(String[] args) {
+		int a=225;
+		double b =2;
+		int x = a/(int)b;
+		char c = 101;
+		char s = (char) x;
+		System.out.println(c+"\n"+s);
+		
+
+	}
+
+}
